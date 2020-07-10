2da8e1e5e2060acdb28fa9130d3e310f36de99f5
Humberto <codycinder@hotmail.com>
2020-07-09 20:13:49 -0500

    Added Lambda expression example

---
+++ functional-programming/src/main/java/finalsection/Lambdas.java
@@ -0,0 +1,21 @@
+package finalsection;
+
+import java.util.function.BiFunction;
+
+public class Lambdas {
+
+	public static void main(String[] args) {
+		
+		BiFunction<String, Integer, String> upperCaseName = (name, age) -> {
+			
+			//logic
+			if(name.isEmpty()) throw new IllegalArgumentException("");
+			System.out.println(age);
+			return name.toUpperCase();
+		};
+		
+		System.out.println(upperCaseName.apply("Alex", 20));
+
+	}
+
+}
